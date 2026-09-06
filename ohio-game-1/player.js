601bf7906d513d1bed27d2e7ffb2a2e0fc333670
windowslnk133-gif <windowslnk133@gmail.com>
2026-09-06 15:48:17 +0800

    Update player.js

--- ohio-game-1/player.js
+++ ohio-game-1/player.js
@@ -10,6 +10,15 @@ class Player {
         this.maxHp = 100;
         this.grounded = false;
         this.hasKey = false;
+
+        // 【新增】隱藏的自動攻擊感應區塊 (Attack Hitbox)
+        this.attackBox = {
+            x: 0,
+            y: 0,
+            width: 160,  // 往前方延伸的感應距離
+            height: 200  // 涵蓋跳躍與垂直高度的感應範圍
+        };
+        this.showHitboxDebug = false; // 如果想在畫面上看到這個隱藏區塊，可以改成 true
     }
 
     reset() {
@@ -43,6 +52,10 @@ class Player {
         if (this.x < 0) this.x = 0;
         if (this.x > 1000 - this.width) this.x = 1000 - this.width;
 
+        // 更新隱藏攻擊區塊的位置：保持在玩家身體前方，且高度居中覆蓋
+        this.attackBox.x = this.x + this.width; // 往右手邊延伸
+        this.attackBox.y = this.y - (this.attackBox.height / 2) + (this.height / 2);
+
         // 平台碰撞檢測
         this.grounded = false;
         platforms.forEach(p => {
@@ -58,7 +71,16 @@ class Player {
     }
 
     draw(ctx, currentSlot, gameState, autoAttackTimer) {
-        // 畫玩家主體
+        // 1. 【除錯用】如果開啟，會畫出淡紅色的隱藏自動攻擊範圍
+        if (gameState === "BOSS" && currentSlot === 1 && this.showHitboxDebug) {
+            ctx.fillStyle = "rgba(255, 0, 0, 0.15)";
+            ctx.fillRect(this.attackBox.x, this.attackBox.y, this.attackBox.width, this.attackBox.height);
+            ctx.strokeStyle = "rgba(255, 0, 0, 0.5)";
+            ctx.lineWidth = 1;
+            ctx.strokeRect(this.attackBox.x, this.attackBox.y, this.attackBox.width, this.attackBox.height);
+        }
+
+        // 2. 畫玩家主體
         ctx.fillStyle = "#00ffcc";
         ctx.fillRect(this.x, this.y, this.width, this.height);
         
@@ -66,10 +88,10 @@ class Player {
         ctx.fillStyle = "#000";
         ctx.fillRect(this.x + 18, this.y + 10, 6, 6);
 
-        // 畫手上拿著的武器或道具
+        // 3. 畫手上拿著的武器或道具
         if (gameState === "BOSS") {
             if (currentSlot === 1) {
-                ctx.fillStyle = "#ffffff"; // 鍵盤外觀
+                ctx.fillStyle = "#ffffff"; // 鍵盤
                 ctx.fillRect(this.x + 22, this.y + 20, 22, 12);
             } else if (currentSlot === 2) {
                 ctx.fillStyle = "#ffff00"; // 手雷
